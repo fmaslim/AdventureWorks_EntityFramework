0fd5a3267504b4bd2eeea7a778e324845b42c1ee
fmasl <fmasl@TABLET-6PB9FQ92>
2021-11-21 03:43:03 -0500

    Add project files.

---
+++ AdventureWorks_EntityFramework/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks_EntityFramework.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+
+		[HttpGet]
+		[Route("EF")]
+		public IEnumerable<Address> GetEF()
+		{
+			using (var dbContext = new AdventureWorks2019Context())
+			{
+				var table = dbContext.Addresses.Take(1).ToList();
+				return table;
+			}
+		}
+	}
+}
